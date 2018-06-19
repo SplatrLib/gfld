288ce70392f0b3a5995cb16287d57c89860f8c3a
Lina <zhukovl@gmail.com>
2018-06-19 17:24:47 -0400

    further updates

--- contentScript.js
+++ contentScript.js
@@ -11,26 +11,40 @@ var header = document.getElementById("Header");
 header.style.width = "100%";
 header.style.backgroundColor =  "rgb(36, 41, 46) !important;";
 
-console.log("now in jquery");
 var tickets = $(".x-grid3-col-hpColHeading_0ticket_type").each(function(){
     var _name = $(this).children("img").attr("title");
     if( _name !== undefined){
         $(this).text(_name);
     }
-    console.log(_name);
 });
-console.log(tickets.length);
 
 
-$.moveColumn = function (table, from, to) {
-    var rows = $('tr', table);
-    var cols;
-    rows.each(function() {
-        cols = $(this).children('th, td');
-        cols.eq(from).detach().insertBefore(cols.eq(to));
-    });
-};
 
-var homepageDiv = $("[id^=homepageGrid]");
-var _table = $(".x-grid3-row-table");
-//$.moveColumn(homepageDiv, 7, 3);
+
+
+
+$(function(){
+    var homepageDiv = $("[id^=homepageGrid]");
+    var tableHeader = $(".x-grid3-hd-row");
+
+    $.moveColumn = function (table, from, to) {
+        var rows = $('tr', table);
+        var cols;
+        rows.each(function() {
+            cols = $(this).children('th, td');
+            cols.eq(from).detach().insertBefore(cols.eq(to));
+        });
+    };
+
+    $.moveColumn(tableHeader, 7, 5);
+    $.moveColumn(homepageDiv, 7, 5);
+
+    $.moveColumn(tableHeader, 7, 9);
+    $.moveColumn(homepageDiv, 7, 9);
+
+    var num_open = $('.x-grid3-col-hpColHeading_status:contains("Open")').empty().addClass("openIcon");
+    var num_test = $('.x-grid3-col-hpColHeading_status:contains("Testing")').empty().addClass("testIcon");
+    var num_dev = $('.x-grid3-col-hpColHeading_status:contains("Development")').empty().addClass("devIcon");
+    var num_appro = $('.x-grid3-col-hpColHeading_status:contains("Approvals")').empty().addClass("appIcon");
+    var num_prod = $('.x-grid3-col-hpColHeading_status:contains("Scheduled for Prod")').empty().addClass("prodIcon");
+});
