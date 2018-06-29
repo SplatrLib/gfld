0988bfcc053c2684299f4b237f2eeccddfbf72ec
Lina <zhukovl@gmail.com>
2018-06-29 11:43:58 -0400

    tooltips

--- js/contentScript.js
+++ js/contentScript.js
@@ -60,7 +60,7 @@
 			//grab the last edited value
 			let $lastEditedOn = $elem.find('.x-grid3-td-hpColHeading_date');
 			const lastEditedOn = $lastEditedOn.children('div:first').text();
-			$lastEditedOn.addClass("req_rm");
+			//$lastEditedOn.addClass("req_rm");
 
 			//set the status icons
 			const $status = $elem.find('.x-grid3-td-hpColHeading_status');
@@ -76,48 +76,51 @@
             //keep the status column
 			$status.addClass("req_fp");
 
-			// Concat system and product into one value
-			const $ticketNumber = $elem.find('.x-grid3-td-hpColHeading_mr');
-			const $title = $elem.find('.x-grid3-td-hpColHeading_title');
-			if ($title.exists()) {
-				const $ticketNumberContent = $ticketNumber.removeAttr('style').children('div:first');
-				const ticketNumber = $ticketNumberContent.text();
-				let $titleElements = $title.children('div:first');
-				let $titleText = $titleElements.children('a[href="#"][onclick]');
-				$titleText.replaceWith($('<div/>').append($titleText.text()));
-				$ticketNumberContent.empty().removeAttr('style').append($('<div/>', { 'class': 'inner_s_class' }).append(`#${ticketNumber}`)).append($('<div/>').append($titleElements));
-
-                //flag the title column to be removed
-				$title.addClass("req_rm");
-			}
-
             //create the 2nd row
-			const $additionalDetailsRow = $('<tr>', {'class': 'displayRow2'});
-			$additionalDetailsRow.append($('<td>'));
+            const $additionalDetailsRow = $('<tr>', {'class': 'displayRow2'});
+            $additionalDetailsRow.append($('<td>'));
 
+            let TICKET_NUMBER = Math.floor(Math.random()*90000) + 10000;;
+
+
+            const $ticketNumber = $elem.find('.x-grid3-td-hpColHeading_mr');
+            const $title = $elem.find('.x-grid3-td-hpColHeading_title');
 			const $system = $elem.find('.x-grid3-td-hpColHeading_System');
 			const $product = $elem.find('.x-grid3-td-hpColHeading_Product');
 			const $department = $elem.find('.x-grid3-td-hpColHeading_Dept');
 			if ($system.exists()) {
-				const system = $system.children('div:first').text();
-				const department = $department.children('div:first').text();
+                const system = $system.children('div:first').text();
+                const product = $product.children('div:first').text();
+                const systemAndProduct = `${system.replace('(WrightFlood / Portal)', '').replace('(AS/400)', '').trim()} | ${product}`
+                let $titleElements = $title.children('div:first');
 
-				const systemAndProduct = `${system.replace('(WrightFlood / Portal)', '').replace('(AS/400)', '').trim()} | ${$product.children('div:first').text()}`;
+                let $titleText = $titleElements.children('a[href="#"][onclick]');
+                $titleText.replaceWith($('<div/>').append($titleText.text()));
 
-				const $td = $('<td/>');
-				$td.append($('<div/>', {'class': 'noFmt'}).append(systemAndProduct));
+                $system.empty().removeAttr('style').append($('<div/>',{class:'inner_s_class'}).append(systemAndProduct));
+                $system.append($titleElements);
+
+
+                const $td = $('<td/>');
+                const department = $department.children('div:first').text();
+                const $ticketNumberContent = $ticketNumber.removeAttr('style').children('div:first');
+                const ticketNumber = $ticketNumberContent.text();
+                TICKET_NUMBER = ticketNumber;
+
+				$td.append($('<div/>', {'class': 'noFmt'}).append('#'+ticketNumber));
 				$td.append($('<div/>', {'class': 'noFmt padLeft'}).append(`Opened by ${department}`));
-				$td.append($('<div/>', {'class': 'noFmt padLeft'}).append(`[ Last Edited On: ${lastEditedOn} ]`));
+				//$td.append($('<div/>', {'class': 'noFmt padLeft'}).append(`[ Last Edited On: ${lastEditedOn} ]`));
 
 				// Replace master ticket icon with name
 				const $ticketType = $elem.find('.x-grid3-td-hpColHeading_0ticket_type');
 				const $ticketTypeImage = $ticketType.find('img[title]');
 				if ($ticketTypeImage.exists()) {
 					const ticketType = $ticketTypeImage.attr('title').replace('\xa0', '');
-
 					if (ticketType && ticketType.includes('Ticket')) {
+					    console.log('adding tt');
 						$td.append($('<a/>', { 'class': 'noFmt padLeft', href: `javascript:goToDetails(${ticketType.match(/\d+/)}, 28);` }).append(`[ ${ticketType} ]`));
 					}
+
 
 					$ticketType.remove();
 				}
@@ -127,10 +130,28 @@
 
 			$additionalDetailsRow.insertAfter($elem);
 
+			$ticketNumber.addClass("req_rm");
+            $title.addClass("req_rm");
+			$system.addClass("req_fp");
+			$product.addClass("req_rm");
+			$department.addClass("req_rm");
 
-			$system.remove();
-			$product.remove();
-			$department.remove();
+
+            //create tooltips out of any remaining colums that have
+            //not been flagged to keep (.req_fp) or remove (.req_rm)
+            var resCard = dataCard($elem, headerMap, TICKET_NUMBER);
+
+            $elem.find('.req_rm').remove();
+
+            $status.addClass('tipped');
+
+            $status.tooltipster({
+                content: resCard,
+                animation: 'fade',
+                delay: 200,
+                theme: 'tooltipster-light',
+                position: 'left'
+            });
 		});
 
 		const headerInfo = [{
@@ -159,4 +180,20 @@
 		const $header = $homePage.find('div[class="x-grid3-header"] tr').empty();
 		headerInfo.forEach((header) => $header.append($headerInfoBar.clone()).append($headerIcon.clone().addClass(header.iconClass)).append($iconText.clone().append(header.text)));
 	});
+
+    function dataCard(row, headerMap, id){
+        let card = $('<div>', {id: (id+'card'), class:'tooltip_templates'});
+        let innercard = $('<div>', {class:"innerCard"});
+        let others = $('td:not(.req_fp,.req_rm)', row).map(function(){
+            let _div = $('div', this);
+            let inner = $('<div>');
+            let _text = _div.text();
+            let _name = headerMap[this.cellIndex];
+            let result =  _name + ": " + _text;
+            innercard.append(inner.append(result));
+        });
+
+        card.append(innercard);
+        return card;
+    }
 })(jQuery);
